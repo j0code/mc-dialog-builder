aa8e308bc54886e558a85f3d91ce5ece5925c6de
j0code <42189560+j0code@users.noreply.github.com>
2025-06-18 15:01:02 +0200

    fix: remove obsolete action from server_links dialog

--- src/data/dialog_type/server_links.ts
+++ src/data/dialog_type/server_links.ts
@@ -3,9 +3,6 @@ import { type NBTCompound } from "../../types.js";
 export default {
 	type: "compound",
 	children: {
-		action: {
-			type: "dialog_action"
-		},
 		exit_action: { type: "dialog_action" },
 		columns: { type: "number", default: 2, min: 1, step: 1 },
 		button_width: { type: "number", default: 150, min: 1, max: 1024, step: 1 },
